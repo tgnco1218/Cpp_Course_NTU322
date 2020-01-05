de7e3d42176ed79b0d0d7e70ed2883d782e1235b
Yi-Ting Kuo <tgnco1218@yahoo.com.tw>
2020-01-05 15:40:15 +0800

    update_HW1

--- CPP322/C5/HW5/HW1PointerPointing/main.cpp
+++ CPP322/C5/HW5/HW1PointerPointing/main.cpp
@@ -20,15 +20,15 @@ int main() {
     cout << "Point_1:" << point_1 << endl;
     cout << "Point_2:" << point_2 << endl;
     
-    cout << "5-1-1 Point_1 = " << point_1 << endl;
-    cout << "5-1-2 *Point_1 = " << *point_1 << endl;
-    cout << "5-1-3 &Point_1 = " << &point_1 << endl;
-    cout << "5-1-4 Point_2 = " << point_2 << endl;
-    cout << "5-1-5 *Point_2 = " << *point_2 << endl;
-    cout << "5-1-6 **Point_2 = " << **point_2 << endl;
-    cout << "5-1-7 &Point_2 = " << &point_2 << endl;
-    cout << "5-1-8 Reference_1 = " << reference_1 << endl;
-    cout << "5-1-9 &Reference_1 = " << &reference_1 << endl;
+    cout << "5-1-1 Point_1 = 0x7ffeefbff548" << endl;
+    cout << "5-1-2 *Point_1 = 99" << endl;
+    cout << "5-1-3 &Point_1 = 0x7ffeefbff540" << endl;
+    cout << "5-1-4 Point_2 = 0x7ffeefbff540" << endl;
+    cout << "5-1-5 *Point_2 = 0x7ffeefbff548" << endl;
+    cout << "5-1-6 **Point_2 = 99" << endl;
+    cout << "5-1-7 &Point_2 cannot know through the given info" << endl;
+    cout << "5-1-8 Reference_1 = 0x7ffeefbff548" << endl;
+    cout << "5-1-9 &Reference_1 = 0x7ffeefbff540" << endl;
     
     return 0;
 }
